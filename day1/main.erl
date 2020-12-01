3bdb035bb76ebd6df1df5be271023d204ec61579
Marcus Birgersson <marcus.s.birgersson@gmail.com>
2020-12-01 13:54:45 +0100

    init day1 commit with makefile for running and compiling and simple code for reading lines from file

--- day1/main.erl
+++ day1/main.erl
@@ -3,4 +3,17 @@
 -export([main/1]).
 
 main([NStr]) ->
-    io:fwrite("Hello, world\n").
+    io:fwrite("File name: ~s \n", [NStr]),
+    Data = readlines(NStr),
+    print_list(Data),
+    halt().
+
+readlines(Fname)->
+    {ok, Raw_Data} = file:read_file(Fname),
+    binary:split(Raw_Data, [<<"\n">>], [global]).
+
+print_list([])-> [];
+print_list([H|T]) ->
+    %% S = erlang:binary_to_list(H),
+    io:format("printing: ~s~n", [H]),
+    print_list(T).
