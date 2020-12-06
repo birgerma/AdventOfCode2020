f1ce2328199f65540ebcb3d018470b61fe5aac72
Marcus Birgersson <marcus.s.birgersson@gmail.com>
2020-12-06 14:06:23 +0100

    add solution for part 2 day6

--- day6/day6.erl
+++ day6/day6.erl
@@ -18,7 +18,55 @@ sol1()->
 
 sol2()->
     Fname="input",
-    Data = tools:readlines(Fname).
+    Data = tools:readlines(Fname),
+    Answers = getAnswearData(Fname),
+    Common=findAllCommonAnswers(Answers,[]),
+    sum(getStringLengths(Common,[]),0).
+    %% findCommonChars(lists:nth(1,Answers)).
+
+findCommon([],_, Result)-> Result;
+findCommon([H|T],List, Result)->
+    IsCommon = isCharInAll(H,List),
+    if
+	IsCommon ->
+	    findCommon(T,List,[H|Result]);
+	true ->
+	    findCommon(T,List,Result)
+    end.
+    %% io:fwrite("Char:~p ~p ~n",[H,Head]).
+
+isCharInAll(Char, [])-> true;
+isCharInAll(Char, [H|T])-> 
+    IsIn = lists:member(Char, H),
+    if
+	IsIn ->
+	    %% io:fwrite("Char: ~p String:~p is in:~p~n",[Char,H,IsIn]),
+	    isCharInAll(Char,T);
+	true  ->
+	    false
+    end.
+    
+
+getShortestString([],Shortest) -> Shortest;
+getShortestString([H|T],"") -> getShortestString(T,H);
+getShortestString([H|T],Shortest) when length(H)<length(Shortest)->getShortestString(T,H);
+getShortestString([H|T],Shortest) -> getShortestString(T,Shortest).
+    
+
+findCommonChars(StringList)->
+    Shortest = getShortestString(StringList,""),
+    Common = findCommon(Shortest, StringList,[]),
+    %% io:fwrite("Strings:~p~n",[StringList]),
+    %% io:fwrite("Shortest: ~p~n",[Shortest]),
+    %% io:fwrite("Common: ~p~n",[Common]),
+    Common.
+
+
+findAllCommonAnswers([],Result) -> lists:reverse(Result);
+findAllCommonAnswers([H|T],Result)->
+    Common = findCommonChars(H),
+    io:fwrite("Answers:~p Common:~p~n",[H,Common]),
+    findAllCommonAnswers(T,[Common|Result]).
 
 
 sum([],Sum)->
