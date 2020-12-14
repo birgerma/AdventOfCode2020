71b9eae51a98d8865b1ea285443f5238b458ff27
Marcus Birgersson <marcus.s.birgersson@gmail.com>
2020-12-14 14:31:12 +0100

    add solution for day14 part 2

--- day14/main.erl
+++ day14/main.erl
@@ -26,7 +26,10 @@ main()->
 %% Correct: 
 sol2()->
     Fname="input",
-    Data = tools:readlines(Fname).
+    Data = tools:readlines(Fname),
+    Prog = formatProgData(Data,[]),
+    Mem = runProgramV2(Prog,"", dict:new()),
+    getMemorySum(Mem).
 
 %% Correct: 
 sol1()->
@@ -46,10 +49,39 @@ getMemorySum(Mem)->
     Values = as_dec(BinValues,[]),
     %% io:fwrite("Decimal values:~w~n",[Values]),
     lists:sum(Values).
+
+getMemorySumV2(Mem)->
+    BinValues = getMemValue(dict:to_list(Mem),[]),
+    Addresses = getMemAddress(dict:to_list(Mem),[]),
+    Factors = countXList(Addresses,[]),
+    Values = as_dec(BinValues,[]),
+    io:fwrite("Memory:~p~n",[BinValues]),
+    io:fwrite("Addresses:~p~n",[Addresses]),
+    io:fwrite("Dec values:~p~n",[Values]),
+    io:fwrite("N Xes:~w~n",[Factors]),
+    %% io:fwrite("Decimal values:~w~n",[Values]),
+    lists:sum(Values).
     
+countXList([],Result)-> lists:reverse(Result);
+countXList([Addr|T],Result)->
+    countXList(T, [countChar(Addr,$X,0)|Result]).
+
+countChar([],_,Count) -> Count;
+countChar([H|T],Char,Count) when H==Char ->
+    countChar(T, Char, Count+1);
+countChar([_|T],Char,Count) ->
+    countChar(T, Char, Count).
+    
+    
+
+
 getMemValue([],Result)-> lists:reverse(Result);
 getMemValue([{Pos, Val}|T],Result)->
     getMemValue(T, [Val|Result]).
+
+getMemAddress([],Result)-> lists:reverse(Result);
+getMemAddress([{Addr, Val}|T],Result)->
+    getMemAddress(T, [Addr|Result]).
 
 runProgram([],Mask,Mem)->Mem;
 runProgram([[M,V]|T],Mask,Mem)->
@@ -60,6 +92,55 @@ runProgram([[M,V]|T],Mask,Mem)->
 runProgram([M|T],Mask, Mem) ->
     %% io:fwrite("Mask change~n"),
     runProgram(T,M,Mem).
+
+
+runProgramV2([],Mask,Mem)->Mem;
+runProgramV2([[M,V]|T],Mask,Mem)->
+    %% io:fwrite("Memory assignment~n"),
+    NewMem = assignV2(M, V, Mask, Mem),
+    %% io:fwrite("New memory:~p~n",[dict:fetch(M,NewMem)]),
+    runProgramV2(T,Mask,NewMem);
+runProgramV2([M|T],Mask, Mem) ->
+    %% io:fwrite("Mask change~n"),
+    runProgramV2(T,M,Mem).
+
+assignV2(Addr, Val, Mask, Mem)->
+    %% io:fwrite("Assigning: Mask=~p~n",[Mask]),
+    %% io:fwrite("Assign Mem[~p]=~p (Mask=~p)~n",[Pos,Val,Mask]),
+    NewAddr = maskAddress(Addr, Mask,[]),
+    %% io:fwrite("Address:~p~n",[NewAddr]),
+    AddressList = generateAddress(NewAddr, "", []),
+    %% io:fwrite("Address List:~p N=~p~n",[AddressList,length(AddressList)]),
+    %% io:fwrite("Final Addr:~p~n",[NewAddr]).
+    multiAssign(AddressList, Val, Mem).
+
+multiAssign([],_, Mem)->Mem;
+multiAssign([Addr|T],Value, Mem)->
+    NewMem = dict:store(Addr, Value,Mem),
+    multiAssign(T,Value, NewMem).
+
+generateAddress([], Addr, Result) ->
+    %% io:fwrite("Ending with addr:~p~n",[Addr]),
+    %% io:fwrite("Result:~p~n",[Result]),
+    lists:reverse([lists:reverse(Addr)|Result]);
+generateAddress([H|T],Addr, Result) when H==$X->
+    %% io:fwrite("Splitting~n"),
+    %% Res1 = generateAddress(T, [$0|Addr], []),
+    Res2 = generateAddress(T, [$1|Addr], lists:append(generateAddress(T, [$0|Addr], []),Result));
+    %% io:fwrite("1:~p 2:~p~n", [Res1, Res2]);
+generateAddress([H|T],Addr, Result) -> 
+    %% io:fwrite("Not x:~c~n",[H]),
+    generateAddress(T,[H|Addr], Result).
+
+
+
+maskAddress([], [],Result) -> lists:reverse(Result);
+maskAddress([A|T1], [M|T2], Result) when M==$0->
+    maskAddress(T1,T2, [A|Result]);
+maskAddress([A|T1], [M|T2], Result) when M==$1->
+    maskAddress(T1,T2, [$1|Result]);
+maskAddress([A|T1], [M|T2], Result) when M==$X->
+    maskAddress(T1,T2, [$X|Result]).
 
 assign(Pos, Val, Mask, Mem)->
     %% io:fwrite("Assigning: Mask=~p~n",[Mask]),
@@ -88,7 +169,7 @@ formatProgData([H|T],Result) ->
     BinVal = as_binary(Val, 36),
     %% io:fwrite("Bin value:~p~n",[BinVal]),
     %% io:fwrite("Match:~p L=~p~n",[{Mem, Val},length([Mem, Val])]),
-    formatProgData(T,[[Mem, BinVal]|Result]).
+    formatProgData(T,[[as_binary(Mem,36), BinVal]|Result]).
 
 
 formatMemAss(A)->
