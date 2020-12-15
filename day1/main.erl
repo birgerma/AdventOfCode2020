f0f125169e6836d74314cbc1d6655197e31abebd
Marcus Birgersson <marcus.s.birgersson@gmail.com>
2020-12-15 22:14:20 +0100

    general cleanup of day 1, add timing

--- day1/main.erl
+++ day1/main.erl
@@ -1,46 +1,47 @@
 
 -module(main).
--export([sol/0, sol1/1, sol2/1]).
+-compile(export_all).
 
-sol()->
-    Fname="input1.txt",
-    sol1(Fname),
-    sol2(Fname).
+main()->
+    Fname="input",
+    Start1 = os:timestamp(),
+    Sol1 = sol1(Fname),
+    Start2 = os:timestamp(),
+    Sol2 = sol2(Fname),
+    EndTime = os:timestamp(),
+    Time1 = timer:now_diff(Start2, Start1)/1000,
+    Time2 = timer:now_diff(EndTime, Start2)/1000,
+    Total = timer:now_diff(EndTime, Start1)/1000,
+
+    io:fwrite("Solution part1:(Time:~f ms): ~p~n", [Time1, Sol1]),
+    io:fwrite("Solution part2 (Time:~f ms): ~p~n", [Time2, Sol2]),
+    io:fwrite("Total execution time: ~f ms~n", [Total]).
 
 sol1(Fname) ->
-    %% io:fwrite("File name: ~s \n", [Fname]),
     Data = stringList2IntList(readlines(Fname)),
-    Res = findPairSum(Data, 2020),
-    io:fwrite("Solution 1: ~p~n", [Res]).
+    findPairSum(Data, 2020).
 
 sol2(Fname) ->
-    %% io:fwrite("File name: ~s \n", [Fname]),
     Data = stringList2IntList(readlines(Fname)),
-    Res = findTrippleSum(Data, 2020),
-    io:fwrite("Solution 2: ~p~n", [Res]).
+    findTrippleSum(Data, 2020).
 
 findPairSum(List,Expected)->
     findPairSum(List, List, Expected).
 findPairSum([H1|_],[H2|_], Expected) when H1+H2==Expected ->
-    %% io:fwrite("~1p * ~1p~n",[H1, H2]),
     H1*H2;
 findPairSum([],[],_) -> -1;
 findPairSum([H|T], [], Expected) -> findPairSum(T, [H|T], Expected);
 findPairSum(List, [_|T], Expected) -> 
-    %% io:fwrite("Left Head: ~1p~n",[H]),
     findPairSum(List, T, Expected).
-
 
 findTrippleSum(List,Expected)->
     findTrippleSum(List, List, List, Expected).
 findTrippleSum([H1|_],[H2|_], [H3|_], Expected) when H1+H2+H3==Expected ->
-    %% io:fwrite("~1p * ~1p * ~1p ~n",[H1, H2, H3]),
     H1*H2*H3;
 findTrippleSum([],[],[], _) -> -1;
 findTrippleSum([H|T], [], List, Expected) -> findTrippleSum(T, [H|T], List,  Expected);
-findTrippleSum(List, [H|T], [], Expected) -> findTrippleSum(List, T, List, Expected);
-findTrippleSum(List1, List2, [H|T], Expected) -> 
-    %% io:fwrite("Left Head: ~1p~n",[H]),
+findTrippleSum(List, [_|T], [], Expected) -> findTrippleSum(List, T, List, Expected);
+findTrippleSum(List1, List2, [_|T], Expected) -> 
     findTrippleSum(List1, List2, T, Expected).
 
 stringList2IntList(List)->
