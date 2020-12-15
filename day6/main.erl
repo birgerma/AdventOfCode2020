53d5bb812523b71e054a40a1131d9c751cda6a63
Marcus Birgersson <marcus.s.birgersson@gmail.com>
2020-12-15 22:58:10 +0100

    day6 general cleanup

--- day6/main.erl
+++ day6/main.erl
@@ -1,25 +1,35 @@
--module(day6).
+-module(main).
 -compile(export_all).
 
 -import('tools', [readlines/1]).
 
 main()->
-    io:fwrite("Solution part1: ~p~n",[sol1()]),
-    io:fwrite("Solution part2: ~p~n",[sol2()]).
-    
+    Fname="input",
+    Start1 = os:timestamp(),
+    Sol1 = sol1(Fname),
+    Start2 = os:timestamp(),
+    Sol2 = sol2(Fname),
+    EndTime = os:timestamp(),
+    Time1 = timer:now_diff(Start2, Start1)/1000,
+    Time2 = timer:now_diff(EndTime, Start2)/1000,
+    Total = timer:now_diff(EndTime, Start1)/1000,
+
+    io:fwrite("Solution part1 (Time:~f ms): ~p~n", [Time1, Sol1]),
+    io:fwrite("Solution part2 (Time:~f ms): ~p~n", [Time2, Sol2]),
+    io:fwrite("Total execution time: ~f ms~n", [Total]).
+
+%% main()->
+%%     io:fwrite("Solution part1: ~p~n",[sol1()]),
+%%     io:fwrite("Solution part2: ~p~n",[sol2()]).
 
 %% Correct: 6782
-sol1()->
-    Fname="input",
-    Data = tools:readlines(Fname),
+sol1(Fname)->
     Answers = getAnswearData(Fname),
     UniqeGroupAns = getUniqueQuestionData(mergeGroupData(Answers,[]),[]),
     sum(getStringLengths(UniqeGroupAns,[]),0).
 
 %% Correct: 3596
-sol2()->
-    Fname="input",
-    Data = tools:readlines(Fname),
+sol2(Fname)->
     Answers = getAnswearData(Fname),
     Common=findAllCommonAnswers(Answers,[]),
     sum(getStringLengths(Common,[]),0).
@@ -34,7 +44,7 @@ findCommon([H|T],List, Result)->
 	    findCommon(T,List,Result)
     end.
 
-isCharInAll(Char, [])-> true;
+isCharInAll(_, [])-> true;
 isCharInAll(Char, [H|T])-> 
     IsIn = lists:member(Char, H),
     if
@@ -44,13 +54,11 @@ isCharInAll(Char, [H|T])->
 	    false
     end.
     
-
 getShortestString([],Shortest) -> Shortest;
 getShortestString([H|T],"") -> getShortestString(T,H);
 getShortestString([H|T],Shortest) when length(H)<length(Shortest)->getShortestString(T,H);
-getShortestString([H|T],Shortest) -> getShortestString(T,Shortest).
+getShortestString([_|T],Shortest) -> getShortestString(T,Shortest).
     
-
 findCommonChars(StringList)->
     Shortest = getShortestString(StringList,""),
     findCommon(Shortest, StringList,[]).
@@ -83,7 +91,6 @@ mergeGroupData([],Result)->
     lists:reverse(Result);
 mergeGroupData([H|T],Result) -> mergeGroupData(T,[mergeStringList(H,"")|Result]).
 
-
 mergeStringList([],Result)->lists:reverse(Result);
 mergeStringList([H|T],Result)->
     mergeStringList(T,H++Result).
@@ -96,4 +103,3 @@ combineGroupData([],Group,Result)->
     lists:reverse([Group|Result]);
 combineGroupData([[]|T], Group,Result) -> combineGroupData(T,[],[Group|Result]);
 combineGroupData([H|T],Group,Result) ->  combineGroupData(T,[H|Group],Result).
-    
