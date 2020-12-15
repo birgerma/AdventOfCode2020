fb047270edc5cf4fb66ed5cfbc78cb63205eb54d
Marcus Birgersson <marcus.s.birgersson@gmail.com>
2020-12-15 22:52:11 +0100

    day5 cleanup

--- day5/main.erl
+++ day5/main.erl
@@ -1,11 +1,26 @@
--module(day5).
+-module(main).
 -compile(export_all).
 
 -import('tools', [readlines/1]).
 
 main()->
-    io:fwrite("Solution part1: ~p~n",[sol1()]),
-    io:fwrite("Solution part2: ~p~n",[sol2()]).
+    Start1 = os:timestamp(),
+    Sol1 = sol1(),
+    Start2 = os:timestamp(),
+    Sol2 = sol2(),
+    EndTime = os:timestamp(),
+    Time1 = timer:now_diff(Start2, Start1)/1000,
+    Time2 = timer:now_diff(EndTime, Start2)/1000,
+    Total = timer:now_diff(EndTime, Start1)/1000,
+
+    io:fwrite("Solution part1 (Time:~f ms): ~p~n", [Time1, Sol1]),
+    io:fwrite("Solution part2 (Time:~f ms): ~p~n", [Time2, Sol2]),
+    io:fwrite("Total execution time: ~f ms~n", [Total]).
+
+
+%% main()->
+%%     io:fwrite("Solution part1: ~p~n",[sol1()]),
+%%     io:fwrite("Solution part2: ~p~n",[sol2()]).
     
 
 %% Correct: 892
@@ -14,6 +29,7 @@ sol1()->
     Data = tools:readlines(Fname),
     getMax(getSeatIds(Data),0).
 
+%% Correct: 
 sol2()->
     Fname="input",
     Data = tools:readlines(Fname),
@@ -23,12 +39,8 @@ sol2()->
 findMissingIds(Ids)->
     findMissingId(lists:sort(Ids)).
 findMissingId([A,B|T]) when A==B-1 -> findMissingId([B|T]);
-findMissingId([A,B|_]) -> 
+findMissingId([A,_|_]) -> 
     A+1.
-
-
-
-    
 
 getSeatIds(Data)->
     SeatData = getAllSeatData(Data, []),
@@ -36,7 +48,7 @@ getSeatIds(Data)->
 
 getMax([], Max)-> Max;
 getMax([H|T],Max) when H>Max-> getMax(T,H);
-getMax([H|T],Max) -> getMax(T,Max).
+getMax([_|T],Max) -> getMax(T,Max).
 
 getAllSeatData([],Result)->
     lists:reverse(Result);
@@ -48,7 +60,6 @@ getSeatData(SeatStr)->
     Row = getSeatRow(SeatStr, 0, 127),
     Col = getSeatCol(SeatStr, 0, 7),
     Id = (Row*8)+Col,
-    %% io:fwrite("Seat: ~p,~p,~p~n",[Row, Col, Id]),
     [Row, Col, Id].
 
 extractListIndexes([],_,Result)->
@@ -56,30 +67,23 @@ extractListIndexes([],_,Result)->
 extractListIndexes([H|T], Index, Result) -> 
     extractListIndexes(T, Index, [lists:nth(Index,H)|Result]).
 
-
-getSeatRow([], Min, Max)-> Min;
+getSeatRow([], Min, _)-> Min;
 getSeatRow([$F|T], Min, Max)->
     NewMax=Min+((Max-Min) div 2),
-    %% io:fwrite("Match F old=[~p ~p] new=[~p ~p] ~n",[Min, Max, Min, NewMax]),
     getSeatRow(T,Min,NewMax);
 getSeatRow([$B|T], Min, Max)->
     NewMin=Min+((Max+1-Min) div 2),
-    %% io:fwrite("Match B old=[~p ~p] new=[~p ~p] ~n",[Min, Max, NewMin,  Max]),
     getSeatRow(T,NewMin,Max);
-getSeatRow([H|T], Min, Max)->
-    %% io:fwrite("Check head=~c~n",[H]),
+getSeatRow([_|T], Min, Max)->
     getSeatRow(T,Min,Max).
 
-getSeatCol([], Min, Max)-> Min;
+getSeatCol([], Min, _)-> Min;
 getSeatCol([$L|T], Min, Max)->
     NewMax=Min+((Max-Min) div 2),
-    %% io:fwrite("Match R old=[~p ~p] new=[~p ~p] ~n",[Min, Max, Min, NewMax]),
     getSeatCol(T,Min,NewMax);
 getSeatCol([$R|T], Min, Max)->
     NewMin=Min+((Max+1-Min) div 2),
-    %% io:fwrite("Match L old=[~p ~p] new=[~p ~p] ~n",[Min, Max, NewMin,  Max]),
     getSeatCol(T,NewMin,Max);
-getSeatCol([H|T], Min, Max)->
-    %% io:fwrite("Check head=~c~n",[H]),
+getSeatCol([_|T], Min, Max)->
     getSeatCol(T,Min,Max).
     
