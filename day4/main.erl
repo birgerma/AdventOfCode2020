1687f44a9362fab9c305846e63e5769b671aecf8
Marcus Birgersson <marcus.s.birgersson@gmail.com>
2020-12-15 22:43:15 +0100

    general cleanup day4

--- day4/main.erl
+++ day4/main.erl
@@ -1,36 +1,40 @@
--module(day4).
+-module(main).
 -compile(export_all).
 
 -import('tools', [readlines/1]).
 
 main()->
-    io:fwrite("Solution part1: ~p~n",[sol1()]),
-    io:fwrite("Solution part2: ~p~n",[sol2()]).
-    
-
-sol2()->
-    %% Fname="example2",
     Fname="input",
+    Start1 = os:timestamp(),
+    Sol1 = sol1(Fname),
+    Start2 = os:timestamp(),
+    Sol2 = sol2(Fname),
+    EndTime = os:timestamp(),
+    Time1 = timer:now_diff(Start2, Start1)/1000,
+    Time2 = timer:now_diff(EndTime, Start2)/1000,
+    Total = timer:now_diff(EndTime, Start1)/1000,
+
+    io:fwrite("Solution part1 (Time:~f ms): ~p~n", [Time1, Sol1]),
+    io:fwrite("Solution part2 (Time:~f ms): ~p~n", [Time2, Sol2]),
+    io:fwrite("Total execution time: ~f ms~n", [Total]).
+
+%% 216 correct
+sol1(Fname)->
+    Data = readPassports(Fname),
+    Valid = checkValidPassports(Data, []),
+    tools:countElements(Valid, true).
+
+%% 150 correct
+sol2(Fname)->
     Data = readPassports(Fname),
     Valid = checkValidPassports(Data, []),
     Validated = getValidatedKeys(Data, Valid, []),
     Correct = validateData(Validated,[]),
     length(Correct).
 
-%% 30 not correct, 215 too low, 216 correct
-sol1()->
-    Fname="input",
-    %% Data = tools:readlines(Fname),
-    Data = readPassports(Fname),
-    Valid = checkValidPassports(Data, []),
-    tools:countElements(Valid, true).
-    %% printTrue(Data, Valid).
-
-
 validateData([],Result)->lists:reverse(Result);
 validateData([H|T], Result) ->
     IsValid = isValidData(H),
-    io:fwrite("Validating: ~p ~p~n",[H,IsValid]),
     if
 	IsValid  ->
 	    validateData(T,[H|Result]);
@@ -38,12 +42,10 @@ validateData([H|T], Result) ->
 	    validateData(T,Result)
     end.
 
-
 isValidData([])->true;
 isValidData([H|T])->
     EntryList = tools:split(H,":"),
     IsValid = isDataValid(lists:nth(1,EntryList),lists:nth(2,EntryList)),
-    %% io:fwrite("Is valid: ~p~n",[IsValid]),
     if
 	IsValid ->
 	    isValidData(T);
@@ -51,27 +53,21 @@ isValidData([H|T])->
 	    false
     end.
 
-
-correctLength(Data,4)->
-    length(Data)==4.
+correctLength(Data,L)->
+    length(Data)==L.
 
 correctRange(Num, Min, Max)->
     (Num>=Min) and (Num=<Max).
 
 isDataValid("byr",Data)->
-    %% L=length(Data),
-    IsOk = correctLength(Data,4) and correctRange(list_to_integer(Data), 1920, 2002),
-    io:fwrite("Validating byr=~p ~p~n",[Data, IsOk]),
-    IsOk;
+    correctLength(Data,4) and correctRange(list_to_integer(Data), 1920, 2002);
 isDataValid("iyr",Data)->
     correctLength(Data,4) and correctRange(list_to_integer(Data), 2010, 2020);
 isDataValid("eyr",Data)->
     correctLength(Data,4) and correctRange(list_to_integer(Data), 2020, 2030);
 
 isDataValid("hgt",Data)->
-    %% io:fwrite("Validating hgt:~p~n",[Data]),
     {Height, Suffix} = extractHeight(Data,[],[]),
-    %% io:fwrite("Unit:~s~n",[Suffix]),
     isValidHeight(Height, Suffix);
 isDataValid("hcl",Data)->
     isValidHairColor(Data);
@@ -80,38 +76,25 @@ isDataValid("ecl",Data)->
     lists:member(Data,Colors);
 isDataValid("pid",Data)->
     Res = re:run(Data, "^\\d{9}$"),
-    io:fwrite("Check pid: ~p ~p ~n",[Data, Res]),
     (Res/=nomatch);
-
-isDataValid(Key,Data)->
-    true.
+isDataValid(_,_)-> true.
 
 isValidHairColor(Color)->
-    %% io:fwrite("Checking ~p ~n",[Color]),
     Res = re:run(Color, "^#[0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f]$"),
     (Res/=nomatch).
-    %% io:fwrite("Checking ~p ~p~n",[Color, IsOk]);
-    %% [Numbers, Letters] = splitInTwo(T,[]),
-    %% io:fwrite("My lists:~p:~p~n",[Numbers, Letters]);
 
 splitInTwo(List,Head) when length(Head)>=length(List) ->
     [Head, lists:reverse(List)];
 splitInTwo([H|T], Head) -> splitInTwo(T, [H|Head]).
 
-
 extractHeight([],Num,Unit) -> 
-    %% io:fwrite("Done:~s ~s~n",[Num,Unit]),
     Value = lists:reverse(Num),
     Suffix = lists:reverse(Unit),
-    %% io:fwrite("Done:~s ~s~n",[Value,Suffix]),
     {list_to_integer(Value),Suffix};
 extractHeight([H|T],Num,Unit) when H==$c;H==$m;H==$i;H==$n-> 
-    %% io:fwrite("Found a unit:~c~n",[H]),
     extractHeight(T, Num, [H|Unit]);
 extractHeight([H|T],Num,Unit) ->
-    %% io:fwrite("Base case, only numbers:~c~n",[H]),
     extractHeight(T, [H|Num], Unit).
-    
     
 isValidHeight(Num,"cm")->
     correctRange(Num, 150, 193);
@@ -124,47 +107,26 @@ getValidatedKeys([_|T1], [false|T2], Result) ->getValidatedKeys(T1,T2,Result);
 getValidatedKeys([H|T1], [_|T2], Result) ->getValidatedKeys(T1,T2,[H|Result]).
     
 readPassports(Fname)->
-    %% Fname="example",
     {ok, Raw_Data} = file:read_file(Fname),
     StrList = re:split(Raw_Data,"\n"),
     List=tools:binList2StrList(StrList),
-    Passports = combineEntries(List,[], []).
+    combineEntries(List,[], []).
 
 combineEntries([],[],Result) ->
     lists:reverse(Result);
 combineEntries([],Current,Result) ->
     combineEntries([], [], [Current|Result]);
 combineEntries([[]|T], Current, Result) -> 
-    %% io:fwrite("Add current ~n",[]),
-    combineEntries(T, [], [Current|Result]);
-combineEntries([[]|T], Current, Result) -> 
-    %% io:fwrite("Add current ~n",[]),
     combineEntries(T, [], [Current|Result]);
 combineEntries([H|T], Current, Result) -> 
     Pass=tools:split(H," "),
     Combined = Current++Pass,
-    %% io:fwrite("To combine:~p and ~p ~n", [Pass, Current]),
-    %% io:fwrite("Result:~p~n",[Combined]),
     combineEntries(T, Combined, Result).
-
-
-printTrue([], _)->done;
-printTrue([H1|T1],[H2|T2]) when H2 ->
-    io:fwrite("valid: ~p~n",[H1]),
-    printTrue(T1,T2);
-printTrue([H1|T1],[H2|T2]) -> printTrue(T1,T2).
-
-    
 
 checkValidPassports([], Result)-> lists:reverse(Result);
 checkValidPassports([H|T], Result) -> checkValidPassports(T, [isValid(H)|Result]).
 
-
-isValid(H)->
-    io:fwrite("Check passport:~p~n",[H]),
-    Pass=H,
-    %% Pass = tools:split(H, " "),
-    %% io:fwrite("Passport:~p~n ", [H]),
+isValid(Pass)->
     if 
 	length(Pass)<7->
 	    false;
@@ -173,7 +135,6 @@ isValid(H)->
     end.
 
 hasAllKeys(Pass)->
-    %% io:fwrite("Passport:~p~n ", [Pass]),
     Keys = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid"],
     hasAllKeys(Pass, 1, Keys).
     
@@ -188,6 +149,4 @@ hasAllKeys(List, Index, [K|T])->
 	    hasAllKeys(List, 1, T);
 	true ->
 	    hasAllKeys(List, Index+1, [K|T])
-    end.
-    
-    
+    end.    
