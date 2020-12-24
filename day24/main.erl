376f4b594942a2f06b0d9e469a571702d249ece0
Marcus Birgersson <marcus.s.birgersson@gmail.com>
2020-12-24 15:25:16 +0100

    add slow solution for day24 part 2

--- day24/main.erl
+++ day24/main.erl
@@ -26,7 +26,13 @@ main()->
 %% Correct: 
 sol2()->
     Fname="input",
-    Data = tools:readlines(Fname).
+    Data = tools:readlines(Fname),
+    Coor = getCoordinates(Data,[]),
+    Floor = flip(Coor,dict:new()),
+    NewFloor = createWhiteNeigbours(Floor),
+    Final = doFlipMoves(NewFloor, 100),
+    countBlack(dict:to_list(Final),0).
+
 
 %% Correct: 
 sol1()->
@@ -49,16 +55,70 @@ test2()->
     Data = tools:readlines(Fname),
     Coor = getCoordinates(Data,[]),
     Floor = flip(Coor,dict:new()),
-    createWhiteNeigbours(Floor).
+    NewFloor = createWhiteNeigbours(Floor),
+    Final = doFlipMoves(NewFloor, 100),
+    countBlack(dict:to_list(Final),0).
+    %% Day1 = flipTiles(NewFloor),
+    %% Day2 = flipTiles(Day1),
+    %% countBlack(dict:to_list(Day2),0).
+
+
+doFlipMoves(Floor, 0)-> Floor;
+doFlipMoves(Floor, Move)->
+    doFlipMoves(flipTiles(createWhiteNeigbours(Floor)),Move-1).
+
+countBlack([],Count)-> Count;
+countBlack([{C,black}|T],Count)-> countBlack(T,Count+1);
+countBlack([{C,V}|T],Count)->
+    countBlack(T,Count).
+    
+
+flipTiles(Floor)->
+    flipTiles(dict:to_list(Floor), Floor,[]).
+
+flipTiles([],Floor, Result)-> dict:from_list(Result);
+flipTiles([{C,V}|T],Floor, Result)->
+    %% io:fwrite("check tile:~p~n",[C]),
+    Debug = false,
+    N = getNeighbours(C),
+    {Black, White} = countNeigbours(N, Floor,{0,0}),
+    tools:print("Pos:~p Color:~p Neigbour: Black=~p White=~p~n",[C, V, Black, White],Debug),
+    if
+	((V==black) and ((Black==0) or (Black>2))) ->
+	    tools:print("Flip to white~n~n",Debug),
+	    flipTiles(T,Floor, [{C,white}|Result]);
+	(V==white) and (Black==2)->
+	    tools:print("Flip to black~n~n",Debug),
+	    flipTiles(T,Floor, [{C,black}|Result]);
+	true ->
+	    tools:print("Dont flip~n~n",Debug),
+	    flipTiles(T,Floor, [{C,V}|Result])
+    end.
+
+
+countNeigbours([], Floor, Result)-> Result;
+countNeigbours([Tile|T], Floor, {Black, White})->
+    IsBlack = (getValue(Tile,Floor)==black),
+    if
+	IsBlack ->
+	    countNeigbours(T, Floor, {Black+1, White});
+	true ->
+	    countNeigbours(T, Floor, {Black, White+1})
+    end.
+		       
 
 createWhiteNeigbours(Floor)->
+    %% io:fwrite("Original:~p~n",[dict:to_list(Floor)]),
     createWhiteNeigbours(dict:to_list(Floor),Floor,[]).
 
 createWhiteNeigbours([],Floor,Result)->
     addToDict(Result,white,Floor);
 createWhiteNeigbours([{C,V}|T],Floor,Result)->
+    Debug = false,
+    tools:print("Find neigbours for ~p~n",[C],Debug),
     N = getNeighbours(C),
     White = filterBlack(N,Floor,[]),
+    tools:print("Neigbours:~p White:~p~n",[N,White],Debug),
     createWhiteNeigbours(T,Floor,White++Result).
 
 addToDict([],Value,Dict)->Dict;
@@ -67,8 +127,9 @@ addToDict([Key|T],Value,Dict)->
     
 filterBlack([],Floor,Result)-> lists:reverse(Result);
 filterBlack([H|T],Floor,Result)->
-    IsBlack = dict:is_key(H,Floor),
-    io:fwrite("Neigbour:~p=~p~n",[H,IsBlack]),
+    %% IsBlack = dict:is_key(H,Floor),
+    IsBlack = (getValue(H,Floor)==black),
+    %% io:fwrite("Neigbour:~p=~p~n",[H,IsBlack]),
     if 
 	IsBlack->
 	    filterBlack(T,Floor, Result);
@@ -76,6 +137,15 @@ filterBlack([H|T],Floor,Result)->
 	    filterBlack(T,Floor, [H|Result])
     end.
     
+getValue(H,Floor)->
+    Exists = dict:is_key(H,Floor),
+    if
+	Exists->
+	    dict:fetch(H,Floor);
+	true ->
+	    white
+    end.
+
 test()->
     I1 = "esenee",
     I2 = "esew",
@@ -86,12 +156,14 @@ test()->
     io:fwrite("Result: ~p ~p ~p ~n",[R1,R2,R3]).
 
 getNeighbours({X,Y})->
-    Diff = [{+1, 0},{-1, 0},{+0.5, 1},{-0.5, 1},{+0.5, -1},{-0.5, -11}],
+    Diff = [{+1, 0},{-1, 0},{+0.5, 1},{-0.5, 1},{+0.5, -1},{-0.5, -1}],
     getNeighbours({X,Y}, Diff, []).
     
 getNeighbours({X,Y}, [], Result)-> lists:reverse(Result);
 getNeighbours({X,Y}, [{DX, DY}|T], Result)->
-    getNeighbours({X,Y}, T, [{X+DX, Y+DY}|Result]).
+    %% getNeighbours({X,Y}, T, [{roundInt(X+DX), roundInt(Y+DY)}|Result]).
+    %% io:fwrite("Neigbour for ~p:~p~n",[])
+    getNeighbours({X,Y}, T, [{roundInt(X+DX), roundInt(Y+DY)}|Result]).
     
 
 flip([], Floor)-> Floor;
